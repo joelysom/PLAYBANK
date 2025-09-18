ddabbda7e6974248e719bedfde688d08a9a8cfc4
JOELYSOM <joelysomalcantaradasilva@gmail.com>
2025-09-18 18:38:00 -0300

    try

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // garante que o Vite resolva os assets corretamente no deploy
+  base: '/', // obrigatório pra Vercel
 })
